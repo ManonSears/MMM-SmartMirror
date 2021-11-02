4fa0f8179278dce4f4b5ce64b96dcbe01a22b5b9
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-02 15:45:44 -0400

    add birds
    birds

--- node_helper.js
+++ node_helper.js
@@ -34,8 +34,61 @@ module.exports = NodeHelper.create({
     }
 
     if (notification === "BIRD") {
-      console.log("Restarting Rpi...")
-      require('child_process').exec('sudo reboot', console.log)
+      Module.register("MMM-HTMLBox",{
+        defaults: {
+          width: "100%",
+          height: "inherit",
+          refresh_interval_sec: 0,
+          file: "birds.html",
+        },
+      
+        start: function() {
+          this.timer = null
+      
+        },
+      
+        notificationReceived: function(noti, payload, sender) {
+          if (noti == "DOM_OBJECTS_CREATED") {
+            this.refresh()
+          }
+        },
+      
+        refresh: function() {
+          if (this.config.file !== "") {
+            this.readFileTrick("/modules/MMM-HTMLBox/" + this.config.file)
+          }
+          this.updateDom()
+          if (this.config.refresh_interval_sec > 0) {
+            var self = this
+            this.timer = setTimeout(function(){
+              self.refresh()
+            }, this.config.refresh_interval_sec * 1000)
+          }
+        },
+      
+        getDom: function() {
+          var wrapper = document.createElement("div")
+          wrapper.innerHTML = this.config.content
+          wrapper.className = "HTMLBX"
+          wrapper.style.width = this.config.width
+          wrapper.style.height = this.config.height
+          return wrapper
+        },
+      
+        readFileTrick: function (url, callback) {
+          var xmlHttp = new XMLHttpRequest()
+          var self = this
+          xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+              console.log("EEE!")
+              self.config.content = xmlHttp.responseText
+              self.updateDom()
+            }
+          }
+          xmlHttp.open("GET", url, true)
+          xmlHttp.send(null)
+        }
+      })
     }
 
   },
