908d7bf4c53942219a71a21d59a260f8e4852781
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-02 10:53:10 -0400

    bird
    changes

--- MMM-SmartMirror.js
+++ MMM-SmartMirror.js
@@ -6,6 +6,8 @@
  * MIT Licensed.
  */
 
+const { fileURLToPath } = require("url");
+
 Module.register("MMM-SmartMirror", {
   defaults: {
   },
@@ -119,7 +121,58 @@ Module.register("MMM-SmartMirror", {
     // Send bird notification when clicked
     birdButtonItem.addEventListener("click",
         () => {
-          window.location.href = 'birds.html'});
+            defaults: {
+            height:"full",
+            width:"full",
+            File: "birds.html",
+            RefreshInterval: 5 * 60 * 1000,
+            scrolling: "no"
+        },
+    
+            start: function () {
+            var self = this;
+            updateDom = self.updateDom(1000)
+          },
+    
+            resume: function() {
+                console.log("Resuming");
+                return this.getDom();
+            },
+            timedRefresh: function(timeoutPeriod){
+              setTimeout("location.reload(true);",timeoutPeriod);
+            },
+    
+            getDom: function() {
+              var iframe = document.createElement("IFRAME");
+              reload = this.config.RefreshInterval
+              refeshPage = this.timedRefresh(reload);
+              iframe.style = "border:0"
+              var repeat = true;
+              while(repeat) {
+                DynHeight = this.config.height;
+                DynWidth = this.config.width;
+              if (DynHeight == "full"){
+                checkHeight = screen.height;
+              }else{
+                checkHeight = this.config.height;
+              }
+              if (DynWidth == "full"){
+                checkWidth = screen.width;
+              }else{
+                checkWidth = this.config.width;
+              }
+                repeat = false;
+              }
+              iframe.width = checkWidth;
+              iframe.height = checkHeight;
+              iframe.allowfullscreen = "true";
+              iframe.mozallowfullscreen = "true";
+              iframe.webkitallowfullscreen = "true";
+                          iframe.scrolling = this.config.scrolling;
+              iframe.src = this.config.url;
+              return iframe;
+            },
+          
     return birdButtonItem
   },
 
